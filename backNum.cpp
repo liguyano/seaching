63ce20174dd039310149b9608a221a10ae8bf10f
liguyano <kurumiwu@foxmail.com>
2022-09-27 13:37:06 +0800

    In the same year

--- backNum.cpp
+++ backNum.cpp
@@ -2,12 +2,29 @@
 // Created by kurum on 9/25/2022.
 //
 #include <iostream>
+#include <string>
 
 using namespace std;
+int reverse(int num)
+{
+    return num/10+num%10*10;
+}
 int main()
 {
     string a;
     cin>>a;
-    cout<<a<<endl;
+    auto centry= stoi(a.substr(0,2));
+    auto reCen= reverse(centry);
+    auto year= stoi(a.substr(2,2));
+    auto reYear= reverse(year);
+    auto mouth= stoi(a.substr(4,2));
+    auto day= stoi(a.substr(6,2));
+    if (reYear<12){
+        if (mouth<reYear)
+        {
+            cout<<reYear-mouth<<" mounth "<<endl;
+            cout<<31-day+reCen<<endl;
+        }
+    }
     return 0;
 }
